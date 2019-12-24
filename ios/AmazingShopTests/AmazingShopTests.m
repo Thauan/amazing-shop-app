bf720d2971fd2bd4439ac874dade06db3e12cd4f
Thauan <thaubr28@gmail.com>
2019-12-24 15:27:51 -0300

    Renamed App

--- ios/AmazingShopTests/AmazingShopTests.m
+++ ios/AmazingShopTests/AmazingShopTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SuperiorShopTests : XCTestCase
+@interface AmazingShopTests : XCTestCase
 
 @end
 
-@implementation SuperiorShopTests
+@implementation AmazingShopTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
