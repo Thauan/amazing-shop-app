38e22de7a02094f99fa93177d96232aa8406ef92
Thauan <thaubr28@gmail.com>
2019-12-30 01:15:04 -0300

    Update Login and Init page

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,17 @@
 import Reactotron from 'reactotron-react-native';
-import { reactotronRedux } from 'reactotron-redux';
+import {reactotronRedux} from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({
+    name: 'Amazing Shop',
+    host: '10.0.92.102',
+  })
     .use(reactotronRedux())
     .use(sagaPlugin())
+    .useReactNative()
+    .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
   tron.clear();
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,35 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-import Main from '~/pages/Main';
+import Login from '~/pages/Login';
 import BootScreen from '~/pages/BootScreen';
+
+const MainStackNavigator = createStackNavigator({
+  Init: {
+    screen: BootScreen,
+    navigationOptions: () => ({
+      headerStyle: {
+        backgroundColor: '#3f966b',
+      },
+      headerTintColor: '#fff',
+      title: 'Amazing Shop',
+    }),
+  },
+  Login: {
+    screen: Login,
+    navigationOptions: () => ({
+      headerStyle: {
+        backgroundColor: '#3f966b',
+      },
+      headerTintColor: '#fff',
+      title: 'Amazing Shop',
+    }),
+  },
+});
 
 const Routes = createAppContainer(
   createSwitchNavigator({
-    BootScreen,
-    Main,
+    Login: MainStackNavigator,
   }),
 );
 
