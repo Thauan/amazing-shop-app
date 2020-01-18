17a9d4f2951599758e1d12ee2598da3139b46f49
Thauan <thaubr28@gmail.com>
2020-01-18 00:58:58 -0300

    duck pattern

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import * as styled from './styles';
+import {StatusBar} from 'react-native';
+
+function Main(props) {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#3f966b" />
+    </>
+  );
+}
+
+export default Main;
